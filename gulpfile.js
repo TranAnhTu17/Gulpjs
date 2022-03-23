4fbf6ec5de66646ab7330ca1447b2eac4b92fae1
TuChan <20020495@vnu.edu.vn>
2022-03-24 00:26:14 +0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const pug = require("gulp-pug");
+const browserSync = require("browser-sync").create();
+
+const options = {
+    pug: {
+        src: "./app/views/**/*.pug",
+        dest: "./public",
+    },
+    styles: {
+        src: "./app/styles/**/*.scss",
+        dest: "./public/css",
+    },
+    scripts: {
+        src: "./app/scripts/*.js",
+        dest: "./public/scripts",
+    },
+};
+function styles() {
+    return gulp
+        .src(options.styles.src)
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest(options.styles.dest))
+        .pipe(
+            browserSync.reload({
+                stream: true,
+            })
+        );
+}
+
+function views() {
+    return gulp
+        .src(options.pug.src)
+        .pipe(pug({ pretty: true }))
+        .pipe(gulp.dest(options.pug.dest))
+        .pipe(
+            browserSync.reload({
+                stream: true,
+            })
+        );
+}
+
+function scripts() {
+    return gulp
+        .src(options.scripts.src)
+        .pipe(gulp.dest(options.scripts.dest))
+        .pipe(
+            browserSync.reload({
+                stream: true,
+            })
+        );
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: "./public",
+        },
+    });
+    gulp.watch(options.styles.src, styles);
+    gulp.watch(options.pug.src, views);
+    gulp.watch(options.scripts.src, scripts);
+}
+
+exports.styles = styles;
+exports.views = views;
+exports.scripts = scripts;
+exports.watch = watch;
